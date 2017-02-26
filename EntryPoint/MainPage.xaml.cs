a8f84e6cf8bb836183b6c6acbba459289209d418
Mateusz Tocha <mati50013@wp.pl>
2017-02-26 15:33:25 +0100

    Lambda Expression

--- EntryPoint/MainPage.xaml.cs
+++ EntryPoint/MainPage.xaml.cs
@@ -27,7 +27,7 @@ namespace EntryPoint
         public MainPage()
         {
             this.InitializeComponent();
-            this.ButtonControl.Click += async delegate (object sender, RoutedEventArgs e)
+            this.ButtonControl.Click += async  (object sender, RoutedEventArgs e) =>
             {
                 var dialog = new MessageDialog("Hello World");
                 await dialog.ShowAsync();
